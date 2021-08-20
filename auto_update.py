b95ac474ae68564843d0b9e752571af1dbedc83c
zero <1584691796@qq.com>
2021-08-20 11:46:34 +0800

    update 2021-08-20 11:46:16.019024

--- auto_update.py
+++ auto_update.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-08-20 10:57:48
-LastEditTime: 2021-08-20 11:41:39
+LastEditTime: 2021-08-20 11:45:52
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /zero-space/auto_update.py
@@ -12,6 +12,7 @@ import datetime
 now=datetime.datetime.now()
 
 cmd=[
+    "hexo s",
     "hexo g",
     "git add .",
     "git commit -m 'update {time}'".format(time=str(now)),
